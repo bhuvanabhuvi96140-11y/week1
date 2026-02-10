24b9c47e39a42eb78614c5d55121298a9fb3fdb8
bhuvanabhuvi96140-11y <bhuvanabhuvi96140@gmail.com>
2026-02-10 17:56:22 +0530

    Add files via upload

---
+++ Day2/personal_details.py
@@ -0,0 +1,13 @@
+name="Bhuvana"
+age=22
+city="Shivamogga"
+Branch="MCA"
+phn_no=9876543219
+email="bhuvanabhui96@gmail.com"
+print("---------Personal_details---------")
+print("Name:",name)
+print("Age:",age)
+print("city:",city)
+print("Branch:",Branch)
+print("Phone Number:",phn_no)
+print("Email:",email)
